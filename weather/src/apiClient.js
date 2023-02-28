d02feaa015cb2150ec806c104145aeea483ec3e1
JamesMDesign <122684936+JamesMDesign@users.noreply.github.com>
2023-02-28 15:54:44 +0000

    files updated

--- weather/src/apiClient.js
+++ weather/src/apiClient.js
@@ -1,4 +1,5 @@
-import axios from 'axios' ;
+import axios from 'axios';
+
 
 export class ApiClient {
 	constructor(location) {
@@ -7,7 +8,7 @@ export class ApiClient {
 	}
 
 	setWeatherLocation(location) {
-		this.getWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=06af2c84a95e6a736fd7bab4b3be279d` ;
+		this.getWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=${process.env.REACT_APP_API_KEY}` ;
 	}
 
   getWeather() {
